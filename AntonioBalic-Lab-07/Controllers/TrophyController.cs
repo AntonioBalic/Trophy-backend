856e47148ff5e9391acaa65461555a3f43688c16
AntonioBalic <92325312+AntonioBalic@users.noreply.github.com>
2025-01-10 17:59:16 +0100

    Refactored repository

--- AntonioBalic-Lab-07/Controllers/TrophyController.cs
+++ AntonioBalic-Lab-07/Controllers/TrophyController.cs
@@ -19,62 +19,60 @@ namespace AntonioBalic_Lab_07.Controllers
         [HttpGet("all")]
         public IEnumerable<Trophy> GetTrophies()
         {
-            return _trophyRepository.Trophy;
+            return _trophyRepository.GetTrophies();
+        //    return _trophyRepository.Trophy;
         }
 
         [HttpPost("new")]
         public IEnumerable<Trophy> AddNewTrophy([FromBody] Trophy trophy)
         {
-            if (trophy.Sponsors == null)
-            {
-                trophy.Sponsors = new List<string>();
-            }
             trophy.Id = Guid.NewGuid();
-            _trophyRepository.Trophy.Add(trophy);
-            return _trophyRepository.Trophy;
+            return _trophyRepository.AddTrophy(trophy);
+            //return _trophyRepository.Trophy;
+
         }
 
         [HttpDelete("delete/{id}")]
         public IEnumerable<Trophy> DeleteTrophy([FromRoute] Guid id)
         {
-            _trophyRepository.Trophy = _trophyRepository.Trophy.Where(x => x.Id != id).ToList();
-            return _trophyRepository.Trophy;
+            //    _trophyRepository.Trophy = _trophyRepository.Trophy.Where(x => x.Id != id).ToList();
+            return _trophyRepository.DeleteTrophy(id);
         }
 
-        [HttpPut("update/{id}")]
-        public IEnumerable<Trophy> UpdateTrophy([FromRoute] Guid id, [FromBody] Trophy updatedTrophy)
-        {
-            var oldTrophy = _trophyRepository.Trophy.FirstOrDefault(x => x.Id == id);
+        //[HttpPut("update/{id}")]
+        //public IEnumerable<Trophy> UpdateTrophy([FromRoute] Guid id, [FromBody] Trophy updatedTrophy)
+        //{
+        //    var oldTrophy = _trophyRepository.Trophy.FirstOrDefault(x => x.Id == id);
 
-            if (oldTrophy == null)
-            {
-                return _trophyRepository.Trophy;
-            }
-            else
-            {
-                oldTrophy.Sportclub = updatedTrophy.Sportclub;
-                oldTrophy.Trophyname = updatedTrophy.Trophyname;
-                oldTrophy.Rank = updatedTrophy.Rank;
-                oldTrophy.Year = updatedTrophy.Year;
-                oldTrophy.Sponsors = updatedTrophy.Sponsors;
+        //    if (oldTrophy == null)
+        //    {
+        //        return _trophyRepository.Trophy;
+        //    }
+        //    else
+        //    {
+        //        oldTrophy.Sportclub = updatedTrophy.Sportclub;
+        //        oldTrophy.Trophyname = updatedTrophy.Trophyname;
+        //        oldTrophy.Rank = updatedTrophy.Rank;
+        //        oldTrophy.Year = updatedTrophy.Year;
+        //        oldTrophy.Sponsors = updatedTrophy.Sponsors;
 
-                return _trophyRepository.Trophy;
-            }
-        }
+        //        return _trophyRepository.Trophy;
+        //    }
+        //}
 
-        // Three endpoints: from route, query and body
+        // Three endpoints: from route, query and body  
         [HttpGet("route/{sportclub}/{trophyname}")]
         public IActionResult GetTrophyFromRoute(string sportclub, string trophyname)
         {
             return Ok($"Sport Club {sportclub} won this year's {trophyname} Trophy :: from route");
         }
-        
+
         [HttpGet("query")]
         public IActionResult GetTrophyFromQuery([FromQuery] string sportclub, [FromQuery] string trophyname)
         {
             return Ok($"Sport Club {sportclub} won this year's {trophyname} Trophy :: from query");
         }
-        
+
         [HttpPost("body")]
         public IActionResult GetTrophyFromBody([FromBody] string trophyname)
         {
--- AntonioBalic-Lab-07/Repositories/TrophyRepository.cs
+++ AntonioBalic-Lab-07/Repositories/TrophyRepository.cs
@@ -1,8 +1,53 @@
-﻿namespace AntonioBalic_Lab_07.Repositories
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AntonioBalic_Lab_07.Repositories
 {
     public class TrophyRepository
     {
-        public List<Trophy> Trophy;
+        private List<Trophy> Trophy;
+
+        public IEnumerable<Trophy> GetTrophies()
+        {
+            return Trophy;
+        }
+
+        public IEnumerable<Trophy> AddTrophy([FromBody] Trophy trophy)
+        {
+            if (trophy.Sponsors == null)
+            {
+                trophy.Sponsors = new List<string>();
+            }
+            trophy.Id = Guid.NewGuid();
+            Trophy.Add(trophy);
+            return Trophy;
+        }
+
+        public IEnumerable<Trophy> DeleteTrophy([FromRoute] Guid id)
+        {
+            Trophy = Trophy.Where(x => x.Id != id).ToList();
+            return Trophy;
+        }
+
+        public IEnumerable<Trophy> UpdateTrophy([FromRoute] Guid id, [FromBody] Trophy updatedTrophy)
+        {
+            var oldTrophy = Trophy.FirstOrDefault(x => x.Id == id);
+
+            if (oldTrophy == null)
+            {
+                return Trophy;
+            }
+            else
+            {
+                oldTrophy.Sportclub = updatedTrophy.Sportclub;
+                oldTrophy.Trophyname = updatedTrophy.Trophyname;
+                oldTrophy.Rank = updatedTrophy.Rank;
+                oldTrophy.Year = updatedTrophy.Year;
+                oldTrophy.Sponsors = updatedTrophy.Sponsors;
+
+                return Trophy;
+            }
+        }
+
         public TrophyRepository()
         {
             Trophy = new List<Trophy>();
